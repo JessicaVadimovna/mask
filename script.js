21c121faa518d9444c119cbbaad7357a00a03537
Jessica <109269190+JessicaVadimovna@users.noreply.github.com>
2024-12-11 05:29:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Функция для обеспечения бесшовности видео
+function updateVideoSize() {
+    const video = document.querySelector('.video-background');
+    const windowRatio = window.innerWidth / window.innerHeight;
+    const videoRatio = video.videoWidth / video.video
+} 
